3fbd2fd2f8cbddd878c5ef216df35d35447f5d75
yimin <yimint@qq.com>
2022-12-31 10:49:24 +0800

    init commit

---
+++ src/test/java/com/yimint/curator/CuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yimint.curator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
